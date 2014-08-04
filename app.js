1c9943e4e3aa6cd51982a851ab65f8a2428d67e4
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-03 22:51:18 -0700

    set up config.js + requires in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  server = http.createServer(app),
+  ejs = require('ejs'),
+  bodyParser = require('body-parser'),
+  Twit = require('twit'),
+  io = require('socket.io').listen(server),
+  config = require('./config/config.js');
+
+server.listen(3000, function(){
+  console.log('server started on localhost:3000');
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
