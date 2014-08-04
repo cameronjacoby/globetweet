e2d9eefb210f07764d954898587c48bd8532cf76
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-03 23:09:48 -0700

    set up routes

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ server.listen(3000, function(){
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function(req, res) {
+  res.render('site/index');
+});
+
+
+app.post('/search', function(req, res) {
+  res.render('site/index');
+});
+
+
+app.get('/*', function(req, res) {
+  res.render('site/404');
+});
