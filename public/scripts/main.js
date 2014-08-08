de93ed8fc9a3ee2e341c3d8fce0aca8d7db9b571
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-07 17:25:30 -0700

    edit socket connection

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,7 +5,7 @@ var map = L.mapbox.map('map', 'cameronjacoby.j4l2ebki')
 
 
 // set variables
-var socket = io('http://localhost'),
+var socket = io(),
 tweetArr = [],
 count = 0,
 geocoder = L.mapbox.geocoder('mapbox.places-v1'),
